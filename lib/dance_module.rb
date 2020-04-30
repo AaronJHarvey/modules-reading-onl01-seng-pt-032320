f762777c3fec5681687ed584e5ab4e6b4ec51b8b
Aaron J Harvey <harvey.aaronj@gmail.com>
2020-04-30 18:13:39 +0000

    Automatically backed up by Learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -1,4 +1,3 @@
-  
 module Dance
   def twirl
     "I'm twirling!"
@@ -15,4 +14,4 @@ module Dance
   def take_a_bow
     "Thank you, thank you. It was a pleasure to dance for you all."
   end
-end
+end 
